2335edde588220469891459ed880e15f7db4d0d1
MHEN2606 <matthew.mahendra@gmail.com>
2023-05-29 20:26:37 +0700

    feat: Update data portofolio

--- src/Data.js
+++ src/Data.js
@@ -22,10 +22,38 @@ const Project = [
     },
     {
         id: 4,
-        name: "3D POINT ANALYZER",
+        name: "Closest Pair of Points for 3D or n-D Points",
         year: "2023",
         author: "Matthew Mahendra",
-        description: "TUCIL STIMA"
+        description: "Finding the closest pair of 3D or n-D points using the divide and conquer algorithm. Made using Python with Matplotlib"
+    },
+    {
+        id: 5,
+        name: "Kita Bisa Jalan-Jalan: A Travel Itinerary App",
+        year: "2023",
+        author: "Matthew Mahendra, Henry Anand S.R., Ahmad Nadil, et. al.",
+        description: "A travel itinerary app made using Python with GUI. A software engineering implementation project"
+    },
+    {
+        id: 6,
+        name: "ShoppingSans",
+        year:"2023",
+        author: "Matthew Mahendra, Henry Anand S.R., Ahmad Nadil, et. al.",
+        description: "A store managing app made with Java and Swing as GUI"
+    },
+    {
+        id: 7,
+        name: "Shortest Path Finding using A* and UCS",
+        year: "2023",
+        author: "Matthew Mahendra and Christophorus Dharma Winata",
+        description: "A shortest path finder using A* and UCS algorithm. Can be used with real locations provided the real coordinates with Java MapViewer. Made using Java."
+    },
+    {
+        id: 8,
+        name: "Simple Chatbot",
+        year: "2023",
+        author: "Matthew Mahendra, Henry Anand S.R., Ahmad Nadil",
+        description: "This is a simple chatbot made using KMP and BM algorithms, also using Regular Expressions. Made with Go Language for the backend and React Typescript for the frontend"
     }
 ]
 
